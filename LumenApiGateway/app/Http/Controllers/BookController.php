d3e85d4c4ac582246fcb9ccebc5669ec83587eaf
AlinaSM <alinasalmen995@gmail.com>
2022-11-07 13:56:38 -0600

    Adding authorServices on BookController

--- LumenApiGateway/app/Http/Controllers/BookController.php
+++ LumenApiGateway/app/Http/Controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Services\BookService;
+use App\Services\AuthorService;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -15,14 +16,21 @@ class BookController extends Controller
      */
     public $bookService;
 
+
+    /**
+     * The service to consume the author service
+     */
+    public $authorService;
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(BookService $bookService)
+    public function __construct(BookService $bookService, AuthorService $authorService)
     {
         $this->bookService = $bookService;
+        $this->authorService = $authorService;
     }
 
 
@@ -40,6 +48,7 @@ class BookController extends Controller
      * @return Illuminate\Http\Response
      */
     public function store(Request $request){
+        $this->authorService->obtainAuthor($request->author_id);
         return $this->successResponse( $this->bookService->createBook($request->all()), Response::HTTP_CREATED);
     }
 
